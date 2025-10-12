7a8828bb0efbebb362b97dff1d67b7333fd1b523
Datto6 <61701645+Datto6@users.noreply.github.com>
2025-10-12 20:48:22 -0300

    busca binaria em lista ordenada de forma circular

---
+++ listaslineares/buscalista_circular.cpp
@@ -0,0 +1,63 @@
+# include <stdio.h>
+
+bool buscaBinaria(int * lista,int elementos,int alvo){
+	int fim=elementos-1;
+	int inicio=0;
+	int meio=(fim+inicio)/2;
+	while(inicio<=fim){
+		if (lista[meio]>alvo){
+			fim=meio-1;
+		}
+		if (lista[meio]<alvo){
+			inicio=meio+1;
+		}
+		if(lista[meio]==alvo){
+			return true;
+		}
+		meio=(inicio+fim)/2;
+	}
+	return false;
+}
+
+int BuscaBinariaCircular(int vec[], int N, int elem, int k){ // parametro k o minimo
+    int ps_fim=N-1; //int fim=(k-2)%N;  
+    int ps_inicio=0; //int inicio=k-1;
+    int ps_meio=(ps_inicio+ps_fim)/2; //int meio=(inicio+(fim+inicio)/2)%N
+    int r_meio=(ps_meio+k)%N;
+    while(ps_inicio<=ps_fim){
+        if (vec[r_meio]==elem){
+            return r_meio;
+        }
+        else if(vec[r_meio]<elem){
+            ps_inicio= ps_meio+1;   // inicio=(N+meio+1)%N;
+            printf("%d inicio ",ps_inicio); 
+        }
+        else{
+            printf("%d fim1 ",ps_fim);
+            ps_fim=ps_meio-1;   //fim=(N+meio-1)%N;
+            printf("%d fim2 ",ps_fim);
+        }
+        printf("%d meio1 ",r_meio);
+        ps_meio=(ps_inicio+ps_fim)/2;
+        r_meio=(ps_meio+k)%N;
+        // meio=(inicio+(fim+inicio)/2)%N;
+        printf("%d meio2 ",r_meio);
+        printf("\n");
+    }
+    if (vec[k-1]==elem){
+        return k-1;
+    }
+    else{
+        return 0;
+    }
+}
+
+int main(void){
+    int A[]={3, 4, 5, 6, 7,8 , 9, 10, 11, 12,13,14, 1, 2};
+    int n=14;
+    int elemento=1;
+    int k=13;
+    int saida=BuscaBinariaCircular(A,n,elemento,k);
+    printf("%d\n",saida);
+    return 0;
+}
