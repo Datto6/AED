3159e0b774a09b553619da5442d7bd2f846a1ae4
Datto6 <61701645+Datto6@users.noreply.github.com>
2025-10-12 21:23:17 -0300

    varias remocoes

--- listaslineares/removes.cpp
+++ listaslineares/removes.cpp
@@ -32,3 +32,42 @@ void Remove(No * &L, No * p) { //remocao de lista simplesmente encadeada, tempo 
 	}
 	free(p);
 }
+
+void RemoveMaiores(No * &L, int k) { //remove todos os elementos maiores que k, encadeada
+	No* p=L;
+	No* pAnt=NULL;
+	while (p!=NULL){
+		if (p->E<=k){
+			pAnt=p;
+			p=p->Prox;
+		}
+		else{
+			if (pAnt==NULL){
+				L=p->Prox;
+			}
+			else{
+				pAnt->Prox=p->Prox;
+			}
+			free(p);
+			if (pAnt==NULL){
+				p=L;
+			}
+			else{
+				p=pAnt->Prox;
+			}
+		}
+
+	}
+}
+void RemoveMaiores(int L[], int &n, int k) { //remove todos maiores que k, sequencial
+	int i=0;
+	while (i<n){ //comeca na posicao 0 ate N
+		if(L[i]>k){  //checo se numero atual e maior que k
+			L[i]=L[n-1];  //remocao sobre escrevendo ultimo valor no atual
+			n--;  //"apagando" ultimo reduzindo indice
+		}
+		else{  //se nao precisa apagar, seguir p proximo
+			i++;
+		}
+	}
+}
