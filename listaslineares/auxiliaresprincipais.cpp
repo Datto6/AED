ea85a9de7e3405a0ac5d492b897e8efb9afdcf7c
Datto6 <61701645+Datto6@users.noreply.github.com>
2025-12-02 19:10:47 -0300

    insere no final

--- listaslineares/auxiliaresprincipais.cpp
+++ listaslineares/auxiliaresprincipais.cpp
@@ -13,3 +13,14 @@ void InsereInicioEnc(No * &L, long long int e) { //funcoes auxiliares
 	No * novo = (No *) malloc(sizeof(No));
 	novo->E = e; novo->Prox = L; L = novo; 
 }
+
+void InsereEnc(No * &L, int e, No * &u) {  //final
+	No * novo = (No *) malloc(sizeof(No));
+	novo->E = e; novo->Prox = NULL; 
+	if (u == NULL) {
+		L = novo; 
+	} else {
+		u->Prox = novo;
+	}
+	u = novo; 
+}
