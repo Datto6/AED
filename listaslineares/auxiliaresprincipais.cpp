7bde27eaffe57b5ff7ac0f90e7ad8e3b493ee35d
Datto6 <61701645+Datto6@users.noreply.github.com>
2025-12-02 19:10:08 -0300

    busca e insere e remove

---
+++ listaslineares/auxiliaresprincipais.cpp
@@ -0,0 +1,15 @@
+int Busca(No* coisa,int x){
+    No* p=coisa;
+    while(p!=NULL){
+        if(p->E==x){
+            return 1; //achou, logo contem e true
+        }
+        p=p->Prox;
+    }
+    return 0; //nao achou, logo nao contem
+}
+
+void InsereInicioEnc(No * &L, long long int e) { //funcoes auxiliares 
+	No * novo = (No *) malloc(sizeof(No));
+	novo->E = e; novo->Prox = L; L = novo; 
+}
